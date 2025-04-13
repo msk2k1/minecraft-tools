eab3c3b722e7d009e111d7f187ba2fc1658dac84
msk2k1 <primary686@gmail.com>
2025-04-13 09:58:14 -0400

    refactor update-server

--- readme.md
+++ readme.md
@@ -2,21 +2,27 @@ These are small scripts I've written to manage local files for Minecraft: Java E
 
 Python is required to run these scripts. Python versions prior to 3.12.5 are untested. No external libraries are needed.
 
-### migrate-instances
+---
+# migrate-instances
 Usually after I install a new Minecraft update, I have to manually copy over my worlds, mods, and texture packs. This script automates that process. The script automatically detects all Minecraft instances, and prompts the user to select the old and new ones. It's assumed the new instance has already been created and launched at least once. `migrate-instances-config.json` should be stored in the same folder as the script, and contains settings intended to be user-defined:
-* Folder containing all Minecraft instances
-* subfolders in instance's .minecraft whose contents should be migrated
-* enable/disable copying options.txt from the source instance.
+- Folder containing all Minecraft instances
+- subfolders in instance's .minecraft whose contents should be migrated
+- enable/disable copying options.txt from the source instance.
 
 After this script runs, the old instance is safe to delete.
 
 Note that installing a mod loader and updating mods must still be done manually.
 
-### update-server
-This is intended to update self-hosted servers (which use server.jar to run) for new Minecraft versions, while preserving the world file, player data, and server properties. It follows steps based on [this tutorial](https://wiki.sportskeeda.com/minecraft/how-to-update-server-minecraft). The script requires paths to the server folder and updated server.jar passed at runtime.
-1. Files to keep after the update are stored in `<server root>/tmp`: the "world" folder, "banned-ips.json", "banned-players.json", "ops.json", "server.properties", "usercache.json", and "whitelist.json".
-2. Files which can be discarded after the update are moved to `<server root>/dump_<current date>`: "logs" folder, "eula.txt", "server.jar", "libraries" folder, and "versions" folder.
+---
+# update-server
+This is intended to update self-hosted servers (which use `server.jar` to run) for new Minecraft versions, while preserving the world file, player data, and server properties. It follows steps based on [this tutorial](https://wiki.sportskeeda.com/minecraft/how-to-update-server-minecraft). 
+
+**Usage:** The script requires paths to the server folder and updated server.jar passed at runtime.
+
+The actions this script performs are:
+1. Files to keep after the update are stored in `<server root>/tmp`
+2. Files which can be discarded after the update are moved to a different folder, `<server root>/dump_<current date>`.
 3. The new server.jar is copied into the server folder and launched for the first time.
-4. The EULA is agreed to.
+4. The EULA is agreed to by modifying `eula.txt`.
 5. Files from `<server root>/tmp` are moved back into the `<server root>`.
-6. The script attempts to grab the user's public IP address and displays it on the screen. If allowed to run, **the user's public IP address will be displayed on the screen.**
+6. By default, the "dump" folder is deleted. (Pass `-k` at runtime to preserve this folder)
--- update-server.py
+++ update-server.py
@@ -1,18 +1,16 @@
-import argparse, shutil, os, time, subprocess, urllib.request, sys
+import argparse, shutil, os, time, subprocess, sys
 
 # given server folder and path to new server.jar
 def getArgs():
     parser = argparse.ArgumentParser()
     parser.add_argument('server', help="Path to the current server folder.")
     parser.add_argument('newJar', help="Path to the updated server.jar.")
+    parser.add_argument('-i', '--show-public-ip', action='store_true', help="Fetch and print public IP address after migrating.")
+    parser.add_argument('-k', '--keep-temp-files', action='store_true', help="Keep folder of items that are no longer necessary after upgrade. (Default behavior is to delete them.)")
 
-    contents = parser.parse_args()
-    output = {
-        "server": os.path.realpath(contents.server),
-        "newJar": os.path.realpath(contents.newJar)
-    }
+    output = parser.parse_args()
 
-    if not output["newJar"].endswith(".jar"): # error condition: provided server file is not .jar
+    if not (str(output.newJar).endswith(".jar")): # error condition: provided server file is not .jar
         raise Exception("Server file must be a .jar file.")
     else: return output
 
@@ -42,7 +40,7 @@ def initServer(jar: str, root: str):
         print("initializing server.jar...")
         subprocess.run(
             ["java", "-jar", activeJar],    # run server jar using "java" command. 
-            cwd=args["server"],             # changes working directory during execution so files are placed in the server folder.
+            cwd=ARGS["server"],             # changes working directory during execution so files are placed in the server folder.
             stdout=subprocess.DEVNULL,      # supress regular terminal output...
             stderr=subprocess.STDOUT)       # ...but not errors
         
@@ -64,9 +62,9 @@ def acceptEULA(root: str):
 
 # copy the files out of tmp folder and attempt to delete it.
 def restoreTmp(root: str):
-    folder = os.path.join(args["server"], "tmp")
+    folder = os.path.join(ARGS["server"], "tmp")
     try:
-        print("Restoring existing server data...")
+        print("Restoring existing serverreturn  data...")
         for i in os.listdir(folder):
             shutil.move(
                 os.path.join(folder, i),
@@ -75,15 +73,17 @@ def restoreTmp(root: str):
     except Exception as e: raise(e)
 
     # delete tmp folder if it is empty
-    if not os.listdir(folder): os.rmdir(os.path.join(args["server"], "tmp"))
+    if not os.listdir(folder): os.rmdir(os.path.join(ARGS["server"], "tmp"))
     else: print("tmp folder could not be deleted because it still contains files.")
 
     return
 
 ### main execution starts here ###
 try:
-    args = getArgs()
-    serverRoot = args["server"]
+    ARGS = getArgs()
+    # serverRoot = ARGS.server
+    print(ARGS.server)
+    quit()
 
     # tmp folder: files to transfer after upgrade
     tmpFolder = os.path.join(serverRoot, "tmp")
@@ -91,28 +91,34 @@ try:
     stashFiles(serverRoot, tmpItems, tmpFolder)
 
     # dump folder: files which can be discarded after upgrade. For safety reasons, the script doesn't delete them itself.
-    dumpFolder = os.path.join(serverRoot, ("dump_" + time.strftime("%Y%m%d-%H%M%S")))
+    dumpName = "dump_" + time.strftime("%Y%m%d-%H%M%S")
+    dumpFolder = os.path.join(serverRoot, dumpName)
     dumpItems = ["logs", "eula.txt", "server.jar", "libraries", "versions"]
     stashFiles(serverRoot, dumpItems, dumpFolder)
 
-    initServer(args["newJar"], serverRoot)
+    initServer(ARGS["newJar"], serverRoot)
     acceptEULA(serverRoot)
     restoreTmp(serverRoot)
 
-    print(f"\nServer upgrade complete. Files in 'dump_' folder can be deleted.")
+    # delete dump folder
+    print("Server upgrade complete.")
     print(f"To launch, run: \ncd {serverRoot}; java -jar ./server.jar --nogui\n")
+    if ARGS.keep_temp_files:
+        print(f"Files in {dumpName} can be safely deleted.")
+    else:
+        try: os.rmdir(dumpFolder)
+        except Exception as e: print(f"dump folder could not be deleted: {e}")
 
     # print IP address to give to friends
-    ipSite = "https://checkip.amazonaws.com"
-    try:
-        myIP = urllib.request.urlopen(ipSite).read().decode('utf8').strip() + ":25565"
-        input("To reveal public IP address, press enter. Otherwise EXIT the program with Ctrl+C or close terminal window.")
-        print("Public server address is:", myIP)
-    except:
-        print(f"Visit {ipSite} to get you public IP. Give this to friends, including :25565 after it.")
-
+    if ARGS.show_public_ip:
+        try:
+            import urllib.request
+            myIP = urllib.request.urlopen("https://checkip.amazonaws.com").read().decode('utf8').strip() + ":25565"
+            print(f"Server address: {myIP}")
+        except Exception as e:
+            print(f"Public IP could not be obtained: {e}")
 except Exception as e: 
     print(e)
-    sys.exit(-1)
+    sys.exit(1)
 
 sys.exit(0)
